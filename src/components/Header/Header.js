3baf47057dcce3e5a786158dfdd2ce7aec8703bf
Marcelle Vargas <marcelle.vargas96@gmail.com>
2023-02-13 14:58:17 -0300

    feat: Add header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import "./Header.scss";
+import logo from "../../assets/img/TravelLogo.png";
+import { GiEarthAmerica, GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <header className="header-container">
+      <div className={`links-container ${(isOpen ? 'expanded' :  '')}`}>
+        <a href="#">blog</a>
+        <a href="#">passeios</a>
+        <a href="#">sobre nós</a>
+        <a href="#">contato</a>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="Travel Logo" />
+      </div>
+      <div className="language-container">
+        <span>PT-BR</span>
+        <GiEarthAmerica />
+      </div>
+      <button className="hamburguer-icon" onClick={() => setOpen(!isOpen)}>
+        <GiHamburgerMenu />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
