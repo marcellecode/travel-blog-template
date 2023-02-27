fb613e347178c70016eac86e4287977a30d2e3e5
marcellecode <marcellecode@gmail.com>
2023-02-27 14:53:59 -0300

    fix: passeios component

--- src/components/Passeios/Passeios.js
+++ src/components/Passeios/Passeios.js
@@ -1,14 +1,16 @@
 import "./Passeios.scss";
 
 const Passeios = ({ image, altText, title, description, buttonLabel }) => {
-  return (
-    <div className="passeios-card">
-      <img src={`${image}`} alt={altText} />
-      <h2>{title}</h2>
-      <span>{description}</span>
-      <button>{buttonLabel}</button>
-    </div>
-  );
+	return (
+		<div className="passeios-card">
+			<img src={`${image}`} alt={altText} />
+			<div className="passeios-content">
+				<h2>{title}</h2>
+				<span>{description}</span>
+				<button>{buttonLabel}</button>
+			</div>
+		</div>
+	);
 };
 
 export default Passeios;
