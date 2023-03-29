9f253eea7c2e181f4126f78629dab6c1070235cb
Marcelle Vargas <marcelle.vargas96@gmail.com>
2023-03-29 10:12:32 -0300

    fix: banner responsive component

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <section className="banner-home">
+      <section className="banner-home">
         <Banner />
       </section>
-      <section className="passeios" id="passeios">
+      {/* <section className="passeios" id="passeios">
         <h1>Passeios</h1>
         <div className="passeios-list">
           <Passeios
